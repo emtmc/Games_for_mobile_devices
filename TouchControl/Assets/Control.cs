08cd03e286433ec9b51684e3f6f080b3cb308570
Eoin McGrath <eoin.mcg96@gmail.com>
2019-02-11 16:06:04 +0000

    selectable object and object manipulation

--- TouchControl/Assets/Control.cs
+++ TouchControl/Assets/Control.cs
@@ -8,42 +8,49 @@ public class Control : MonoBehaviour
     private bool dragging = false;
     private bool pinch = false;
     private Transform toDrag;
- 
-    void Update() {
-        Vector3 position;
- 
-        if (Input.touchCount < 1) {
-            dragging = false; 
-            return;
-        }
- 
+    private bool isTap;
+    public Selector cam;
+
+    void Update()
+    {
+       Vector3 position;    
+                   
+       if (Input.touchCount < 1) 
+       {
+           dragging = false; 
+           return;
+       }
+
         var touch = Input.touches[0];
         Vector2 pos = touch.position;
- 
-        if(touch.phase == TouchPhase.Began) {
+
+        if (touch.phase == TouchPhase.Began)
+        {
             RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(pos); 
+            Ray ray = Camera.main.ScreenPointToRay(pos);
             
-            if(Physics.Raycast(ray, out hit))
-            {
-                toDrag = hit.transform;
-                dist = hit.transform.position.z - Camera.main.transform.position.z;
-                dragging = true;
-                if (Input.touchCount == 2)
+                if(Physics.Raycast(ray, out hit))
                 {
-                    touch = Input.GetTouch(1);
+                   // Scale objectTouched = hit.collider.GetComponent<Scale>();
+                   // if (objectTouched) objectTouched.Update();                 
+                    toDrag = hit.transform;
+                    dist = hit.transform.position.z - Camera.main.transform.position.z;
+             
+                    dragging = true;
+                    if (Input.touchCount == 2)
+                    {
+                        touch = Input.GetTouch(1);
+                    }
                 }
-            }
-           
+    
         }
         if (dragging && touch.phase == TouchPhase.Moved) {
-            position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-            position = Camera.main.ScreenToWorldPoint(position);
-            toDrag.position = position;
+           position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+           position = Camera.main.ScreenToWorldPoint(position);
+           toDrag.position = position;
         }
         if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
             dragging = false;
         }
-  
     }
 }
